491bc3cc5b4a08fced4d743d2b8c370d4a079066
zinospetrel <zinospetrel@proton.me>
2025-10-25 16:09:58 +0700

    Update ubuntu_x86_64.sh

--- softs/termux/ubuntu_x86_64.sh
+++ softs/termux/ubuntu_x86_64.sh
@@ -24,7 +24,7 @@ if [ "$CMD" == "install" ]; then
 
   echo "DISTRO_ARCH=x86_64" > $PREFIX/etc/proot-distro/ubuntu-x86_64.sh
   echo "PROOT_DISTRO_DEBUG=1" >> $PREFIX/etc/proot-distro/ubuntu-x86_64.sh
-  echo "PROOT_DISTRO_QEMU_BINARY=qemu-x86_64" >> $PREFIX/etc/proot-distro/ubuntu-x86_64.sh
+  echo "PROOT_DISTRO_QEMU_BINARY=qemu-x86-64" >> $PREFIX/etc/proot-distro/ubuntu-x86_64.sh
   echo -e "DISTRO_NAME=\"Ubuntu (25.10)\"" >> $PREFIX/etc/proot-distro/ubuntu-x86_64.sh
   echo -e "DISTRO_COMMENT=\"Regular release (questing).\"" >> $PREFIX/etc/proot-distro/ubuntu-x86_64.sh
   echo -e "TARBALL_URL['x86_64']=\"https://github.com/termux/proot-distro/releases/download/v4.30.1/ubuntu-questing-x86_64-pd-v4.30.1.tar.xz\"" >> $PREFIX/etc/proot-distro/ubuntu-x86_64.sh
