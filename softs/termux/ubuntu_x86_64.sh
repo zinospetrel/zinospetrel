ae5751495c35c05c47fcd04104f26777a01186dc
zinospetrel <zinospetrel@proton.me>
2025-10-26 08:04:15 +0700

    Update ubuntu_x86_64.sh

--- softs/termux/ubuntu_x86_64.sh
+++ softs/termux/ubuntu_x86_64.sh
@@ -35,7 +35,7 @@ if [ "$CMD" == "install" ]; then
 distro_setup() {
 	echo -e \"Configure en_US.UTF-8 locale.\"
 	sed -i -E 's/#[[:space:]]?(en_US.UTF-8[[:space:]]+UTF-8)/\1/g' ./etc/locale.gen
-	run_proot_cmd DEBIAN_FRONTEND=noninteractive dpkg-reconfigure locales
+	run_proot_cmd dpkg-reconfigure locales
 	echo -e \"Configure en_US.UTF-8 locale. -- Finished.\"
 }
 "  >> $PREFIX/etc/proot-distro/ubuntu-x86_64.sh
