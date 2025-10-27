ba8ae4488622609764dff8263fdc00a3bbe37083
zinospetrel <zinospetrel@proton.me>
2025-10-28 05:26:55 +0700

    Update wks-01.sh

--- softs/wks/wks-01.sh
+++ softs/wks/wks-01.sh
@@ -101,12 +101,100 @@ if [ "$CMD" == "" ]; then
 fi
 
 if [ "$CMD" == "install" ]; then
-  exec "$HOME_DIR/wgb/wks01a_install"
+  echo -n -e "\u001b[2J"
+  echo -e "===================================="
+  echo -e "         _  Web+Gopher Kiosk Network" 
+  echo -e " __ __ _(_)__ _ ___ ___ _ _  ╋╋┓┏┓┏┓"    
+  echo -e " \\ V  V / / _\` / -_) _ \\ ' \\ ┃┃┃┫ ┗┓" 
+  echo -e "  \\_/\\_/|_\\__, \\___\\___/_||_|╋╋┛┗┛┗┛" 
+  echo -e "   P-01    |___/ V1: 2025.11.09_17.00"
+  echo -e "===================================="
+  echo -e "    Non-touch Kiosk for Android"
+  echo -e "===================================="
+  echo -e ""
+  echo -e "===================================="
+  echo -e "              INSTALL               "              
+  echo -e "   ------------------------------   "   
+  echo -e " "
+  echo -e " + PATH: $ME_FL "
+  echo -e " "
+
+  pkg update -y && pkg upgrade -y
+
+  pkg install -y proot-distro
+
+  mkdir -p $HOME_DIR/wgb
+  chmod u+x $HOME_DIR/wgb
+  chmod u+r $HOME_DIR/wgb
+  chmod g+x $HOME_DIR/wgb
+  chmod g+r $HOME_DIR/wgb
+  chmod o+x $HOME_DIR/wgb
+  chmod o+r $HOME_DIR/wgb
+  
+  curl -o $HOME_DIR/wgb/qemu-x86_64-static -sL -H 'Cache-Control: no-cache, no-store' --noproxy "*" https://github.com/multiarch/qemu-user-static/releases/download/v7.2.0-1/qemu-x86_64-static # (Adjust version as needed)
+  chmod u+w $HOME_DIR/wgb/qemu-x86_64-static
+  chmod u+r $HOME_DIR/wgb/qemu-x86_64-static
+  chmod u+x $HOME_DIR/wgb/qemu-x86_64-static
+  chmod g-x $HOME_DIR/wgb/qemu-x86_64-static
+  chmod g+r $HOME_DIR/wgb/qemu-x86_64-static
+  chmod o-x $HOME_DIR/wgb/qemu-x86_64-static
+  chmod o+r $HOME_DIR/wgb/qemu-x86_64-static
+
+  echo 'export PATH="$HOME/wgb:$PATH"' >> ~/.bashrc
+  source ~/.bashrc
+	
+  echo "DISTRO_ARCH=x86_64" > $PREFIX/etc/proot-distro/zpd-wks-01.sh
+  echo "PROOT_DISTRO_DEBUG=1" >> $PREFIX/etc/proot-distro/zpd-wks-01.sh
+  echo "PROOT_DISTRO_QEMU_BINARY=$HOME/wgb/qemu-x86_64-static" >> $PREFIX/etc/proot-distro/zpd-wks-01.sh
+  echo -e "DISTRO_NAME=\"Wigeon#KS-01 on Ubuntu (24.04.3 LTS)\"" >> $PREFIX/etc/proot-distro/zpd-wks-01.sh
+  echo -e "DISTRO_COMMENT=\"24.04.3 LTS (Noble).\"" >> $PREFIX/etc/proot-distro/zpd-wks-01.sh
+  echo -e "TARBALL_URL['x86_64']=\"https://github.com/termux/proot-distro/releases/download/v4.11.0/ubuntu-noble-x86_64-pd-v4.11.0.tar.xz\"" >> $PREFIX/etc/proot-distro/zpd-wks-01.sh
+  echo -e "TARBALL_SHA256['x86_64']=\"f024b1e17413737d8b385d22736d2e3eb2af9ba665fdbda1277bcca8f397e5a2\"" >> $PREFIX/etc/proot-distro/zpd-wks-01.sh
+  echo -e "
+distro_setup() {
+	echo -e \"Configure en_US.UTF-8 locale...\"
+	sed -i -E 's/#[[:space:]]?(en_US.UTF-8[[:space:]]+UTF-8)/\1/g' ./etc/locale.gen
+	run_proot_cmd dpkg-reconfigure locales
+	echo -e \"Configure en_US.UTF-8 locale... -- Finished.\"
+}
+"  >> $PREFIX/etc/proot-distro/zpd-wks-01.sh
+
+  pkg install termux-api -y
+
+  termux-open-url "https://play.google.com/store/apps/details?id=com.iiordanov.freebVNC"
+
+  termux-open-url "https://play.google.com/store/apps/details?id=com.gazlaws.codeboard"
+
+  proot-distro install zpd-wks-01 --override-arch x86_64
+
+  proot-distro copy $ME_FL zpd-wks-01:/root/wks-01.bh
+
+  proot-distro copy $HOME_DIR/wgb/wigeon#ks-01-x zpd-wks-01:/bin/wigeon#ks-01-x
+
+  proot-distro login zpd-wks-01 -- /root/wks-01.bh configure
   exit
 fi
 
 if [ "$CMD" == "uninstall" ]; then
-  exec "$HOME_DIR/wgb/wks01a_uninstall"
+  echo -n -e "\u001b[2J"
+  echo -e "===================================="
+  echo -e "         _  Web+Gopher Kiosk Network" 
+  echo -e " __ __ _(_)__ _ ___ ___ _ _  ╋╋┓┏┓┏┓"    
+  echo -e " \\ V  V / / _\` / -_) _ \\ ' \\ ┃┃┃┫ ┗┓" 
+  echo -e "  \\_/\\_/|_\\__, \\___\\___/_||_|╋╋┛┗┛┗┛" 
+  echo -e "   P-01    |___/ V1: 2025.11.09_17.00"
+  echo -e "===================================="
+  echo -e "    Non-touch Kiosk for Android"
+  echo -e "===================================="
+  echo -e ""
+  echo -e "===================================="
+  echo -e "              UNINSTALL             "              
+  echo -e "   ------------------------------   "   
+  echo -e " "
+  echo -e " + PATH: $ME_FL "
+  echo -e " "
+  
+  proot-distro remove zpd-wks-01 
   exit
 fi
 
