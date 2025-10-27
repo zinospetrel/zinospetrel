7f00453dc39013d54ed34cf00a93dfdda283a561
zinospetrel <zinospetrel@proton.me>
2025-10-27 16:23:03 +0700

    Update wks-01.sh

--- softs/wks/wks-01.sh
+++ softs/wks/wks-01.sh
@@ -39,16 +39,16 @@ if [ "$CMD" == "" ]; then
   echo -e " "
   echo -e " + PATH: $ME_FL "
   echo -e " "
-  mkdir -p "~/bin"
+  mkdir -p ~/bin
   wget -O "wks-01.bh" --no-cache "https://tinyurl.com/zpd-wks-01"
   chmod u+x $ME_FL
   chmod g+x $ME_FL
   chmod o+x $ME_FL
   
-  wget -O "~/bin/wigeon#ks-01-a" --no-cache "https://github.com/zinospetrel/zinospetrel/raw/refs/heads/main/softs/wks/wks-01/wigeon%23ks-01-a"
-  chmod u+x "~/bin/wigeon#ks-01-a"
-  chmod g+x "~/bin/wigeon#ks-01-a"
-  chmod o+x "~/bin/wigeon#ks-01-a"
+  wget -O ~/bin/wigeon#ks-01-a --no-cache "https://github.com/zinospetrel/zinospetrel/raw/refs/heads/main/softs/wks/wks-01/wigeon%23ks-01-a"
+  chmod u+x ~/bin/wigeon#ks-01-a
+  chmod g+x ~/bin/wigeon#ks-01-a
+  chmod o+x ~/bin/wigeon#ks-01-a
   cd ~/bin && ln -s "wigeon#ks-01-a" "wks01a_help"
   cd ~/bin && ln -s "wigeon#ks-01-a" "wks01a_install"
   cd ~/bin && ln -s "wigeon#ks-01-a" "wks01a_uninstall"
