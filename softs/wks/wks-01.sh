9f0811421f0f5e8dd70d359fad28e03322c63414
zinospetrel <zinospetrel@proton.me>
2025-10-27 15:39:06 +0700

    Update wks-01.sh

--- softs/wks/wks-01.sh
+++ softs/wks/wks-01.sh
@@ -39,97 +39,29 @@ if [ "$CMD" == "" ]; then
   echo -e " "
   echo -e " + PATH: $ME_FL "
   echo -e " "
+  mkdir -p "~/bin"
   wget -O "wks-01.bh" "https://tinyurl.com/zpd-wks-01"
   chmod u+x $ME_FL
   chmod g+x $ME_FL
   chmod o+x $ME_FL
+  wget -O "~/bin/wigeon#ks-01-a" "https://github.com/zinospetrel/zinospetrel/raw/refs/heads/main/softs/wks/wks-01/wigeon%23ks-01-a"
+  chmod u+x "~/bin/wigeon#ks-01-a"
+  chmod g+x "~/bin/wigeon#ks-01-a"
+  chmod o+x "~/bin/wigeon#ks-01-a"
+  ln -s "~/bin/wigeon#ks-01-a" "~/bin/wks01a_help"
+  ln -s "~/bin/wigeon#ks-01-a" "~/bin/wks01a_install"
+  ln -s "~/bin/wigeon#ks-01-a" "~/bin/wks01a_uninstall"
+  
   $ME_FL "install"
   exit
 fi
 
 if [ "$CMD" == "install" ]; then
-  echo -n -e "\u001b[2J"
-  echo -e "===================================="
-  echo -e "         _  Web+Gopher Kiosk Network" 
-  echo -e " __ __ _(_)__ _ ___ ___ _ _  ╋╋┓┏┓┏┓"    
-  echo -e " \\ V  V / / _\` / -_) _ \\ ' \\ ┃┃┃┫ ┗┓" 
-  echo -e "  \\_/\\_/|_\\__, \\___\\___/_||_|╋╋┛┗┛┗┛" 
-  echo -e "   P-01    |___/ V1: 2025.11.09_17.00"
-  echo -e "===================================="
-  echo -e "    Non-touch Kiosk for Android"
-  echo -e "===================================="
-  echo -e ""
-  echo -e "===================================="
-  echo -e "              INSTALL               "              
-  echo -e "   ------------------------------   "   
-  echo -e " "
-  echo -e " + PATH: $ME_FL "
-  echo -e " "
-  
-  pkg update -y && pkg upgrade -y
-
-  pkg install wget -y
-
-  pkg install -y proot-distro
-
-  mkdir -p ~/bin
-  curl -Lo ~/bin/qemu-x86_64-static https://github.com/multiarch/qemu-user-static/releases/download/v7.2.0-1/qemu-x86_64-static # (Adjust version as needed)
-  chmod +x ~/bin/qemu-x86_64-static
-
-  echo 'export PATH="$HOME/bin:$PATH"' >> ~/.bashrc
-  source ~/.bashrc
-	
-  echo "DISTRO_ARCH=x86_64" > $PREFIX/etc/proot-distro/zpd-wks-01.sh
-  echo "PROOT_DISTRO_DEBUG=1" >> $PREFIX/etc/proot-distro/zpd-wks-01.sh
-  echo "PROOT_DISTRO_QEMU_BINARY=qemu-x86-64" >> $PREFIX/etc/proot-distro/zpd-wks-01.sh
-  echo -e "DISTRO_NAME=\"Wigeon#KS-01 on Ubuntu (24.04.3 LTS)\"" >> $PREFIX/etc/proot-distro/zpd-wks-01.sh
-  echo -e "DISTRO_COMMENT=\"24.04.3 LTS (Noble).\"" >> $PREFIX/etc/proot-distro/zpd-wks-01.sh
-  echo -e "TARBALL_URL['x86_64']=\"https://github.com/termux/proot-distro/releases/download/v4.11.0/ubuntu-noble-x86_64-pd-v4.11.0.tar.xz\"" >> $PREFIX/etc/proot-distro/zpd-wks-01.sh
-  echo -e "TARBALL_SHA256['x86_64']=\"f024b1e17413737d8b385d22736d2e3eb2af9ba665fdbda1277bcca8f397e5a2\"" >> $PREFIX/etc/proot-distro/zpd-wks-01.sh
-  echo -e "
-distro_setup() {
-	echo -e \"Configure en_US.UTF-8 locale...\"
-	sed -i -E 's/#[[:space:]]?(en_US.UTF-8[[:space:]]+UTF-8)/\1/g' ./etc/locale.gen
-	run_proot_cmd dpkg-reconfigure locales
-	echo -e \"Configure en_US.UTF-8 locale... -- Finished.\"
-}
-"  >> $PREFIX/etc/proot-distro/zpd-wks-01.sh
-
-  pkg install termux-api -y
-
-  termux-open-url "https://play.google.com/store/apps/details?id=com.iiordanov.freebVNC"
-
-  termux-open-url "https://play.google.com/store/apps/details?id=com.gazlaws.codeboard"
-
-  proot-distro install zpd-wks-01
-
-  proot-distro copy $ME_FL zpd-wks-01:/root/wks-01.bh
-
-  proot-distro login zpd-wks-01 -- /root/wks-01.bh configure
-
-  #proot-distro login zpd-wks-01
+  ~/bin/wks01a_install
 fi
 
 if [ "$CMD" == "uninstall" ]; then
-  echo -n -e "\u001b[2J"
-  echo -e "===================================="
-  echo -e "         _  Web+Gopher Kiosk Network" 
-  echo -e " __ __ _(_)__ _ ___ ___ _ _  ╋╋┓┏┓┏┓"    
-  echo -e " \\ V  V / / _\` / -_) _ \\ ' \\ ┃┃┃┫ ┗┓" 
-  echo -e "  \\_/\\_/|_\\__, \\___\\___/_||_|╋╋┛┗┛┗┛" 
-  echo -e "   P-01    |___/ V1: 2025.11.09_17.00"
-  echo -e "===================================="
-  echo -e "    Non-touch Kiosk for Android"
-  echo -e "===================================="
-  echo -e ""
-  echo -e "===================================="
-  echo -e "              UNINSTALL             "              
-  echo -e "   ------------------------------   "   
-  echo -e " "
-  echo -e " + PATH: $ME_FL "
-  echo -e " "
-  
-  proot-distro remove zpd-wks-01 
+  ~/bin/wks01a_uninstall
 fi
 
 if [ "$CMD" == "configure" ]; then
