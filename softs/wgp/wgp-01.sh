83833172885d8071b81af587363278ade822a11b
zinospetrel <zinospetrel@proton.me>
2025-10-28 13:49:00 +0700

    Update wgp-01.sh

--- softs/wgp/wgp-01.sh
+++ softs/wgp/wgp-01.sh
@@ -211,9 +211,9 @@ EOF
   rm -f $HOME_DIR/wgb/.bashrc
   rm -f $HOME_DIR/wgb/.bashrc.org
 
-  proot-distro login --no-kill-on-exit zpd-wgp-01 -- /bin/bash -c "cd /root/wgp01 && ./wgp_stop && ./wgp_start& ; sleep 2; exit;"
-
-  sleep 15
+  proot-distro login --no-kill-on-exit zpd-wgp-01 -- /bin/bash -c "cd /root/wgp01; ./wgp_start& echo 'Press Enter to continue ... '; read -n 1 -s v_key; exit;"
+  echo -e "\nPress Enter to continue ... "
+  read -n 1 -s v_key
   
   termux-open-url "http://localhost.localdomain:8619"
   exit
@@ -271,13 +271,17 @@ if [ "$CMD" == "config" ]; then
 fi
 
 if [ "$CMD" == "start" ]; then
-  proot-distro login --no-kill-on-exit zpd-wgp-01 -- /bin/bash -c "cd /root/wgp01 && ./wgp_stop && ./wgp_start& ; sleep 2; exit;"
-  sleep 15
+  proot-distro login --no-kill-on-exit zpd-wgp-01 -- /bin/bash -c "cd /root/wgp01; ./wgp_start& echo 'Press Enter to continue ... '; read -n 1 -s v_key; exit;"
+  echo -e "\nPress Enter to continue ... "
+  read -n 1 -s v_key
+  
   termux-open-url "http://localhost.localdomain:8619"
   exit
 fi
 
 if [ "$CMD" == "stop" ]; then
-  proot-distro login --no-kill-on-exit zpd-wgp-01 -- /bin/bash -c "cd /root/wgp01 && ./wgp_stop; sleep 2; exit;"
+  proot-distro login --no-kill-on-exit zpd-wgp-01 -- /bin/bash -c "cd /root/wgp01; ./wgp_stop& echo 'Press Enter to continue ... '; read -n 1 -s v_key; exit;"
+  echo -e "\nPress Enter to continue ... "
+  read -n 1 -s v_key
   exit
 fi
