03a2663a99223a67775fbb53a2382ff19cf33b30
zinospetrel <zinospetrel@proton.me>
2025-10-29 21:19:34 +0700

    Add files via upload

--- softs/wgp/wgp-01/wgp-01.c
+++ softs/wgp/wgp-01/wgp-01.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
-#include <string.h>
 #include <unistd.h>
-#include <time.h>
 #include <termios.h>
-#include <ctype.h>
 
 int getch();
 int getche();
